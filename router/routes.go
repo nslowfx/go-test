9276f1f10f54a640c35e768217586455a5d7e990
nslowfx <slxxwz@gmail.com>
2025-07-21 02:39:34 -0300

    feat: add show opening handler logic

--- router/routes.go
+++ router/routes.go
@@ -2,14 +2,18 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	docs "github.com/nslowfx/go-test/docs"
 	"github.com/nslowfx/go-test/handler"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(r *gin.Engine) {
 	// Initialize Handler
 	handler.InitializeHandler()
-
-	v1 := r.Group("/v1/")
+	basePath := "/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := r.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -17,4 +21,7 @@ func initializeRoutes(r *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+
+	// Initialize Swagger
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
